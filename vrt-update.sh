fe0524cea804bcc1f189d902c316179fb27c759d
Jason Ish <ish@unx.ca>
2013-06-07 14:08:22 -0600

    Script to update certain files from the VRT site.

---
+++ vrt-update.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+#
+# Script to update configuration files from the VRT site:
+#     http://www.snort.org/vrt/snort-conf-configurations
+
+for directory in $(find * -maxdepth 1 -type d -name etc); do
+    version=$(dirname ${directory} | tr -d .)
+
+    for filename in snort.conf \
+	classification.config \
+	reference.config \
+	gen-msg.map
+    do
+	echo "Updating ${directory}/${filename}."
+	url="http://labs.snort.org/snort/${version}/${filename}"
+	if ! curl --progress-bar --fail -o ${directory}/${filename} ${url}
+	then
+	    echo "ERROR: Failed to get ${url}."
+	    exit 1
+	fi
+    done
+done
